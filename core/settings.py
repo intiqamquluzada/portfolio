cec0fe5ada709e6dd5779e076b4276fb8319e652
intiqamquluzada <quluzadintiqam@gmail.com>
2023-11-05 00:59:16 +0400

    db false=

--- core/settings.py
+++ core/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': str(os.getenv('POSTGRES_DB')),
+            'USER': str(os.getenv('POSTGRES_USER')),
+            'PASSWORD': str(os.getenv('POSTGRES_PASSWORD')),
+            'HOST': str(os.getenv('POSTGRES_HOST')),
+            'PORT': '5432',
+        }}
 
 
 # Password validation
